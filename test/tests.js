5e589b79d132a1ac38e7de2d8da5f12bac0943fc
Lucia Brammer <brammerl96@gmail.com>
2020-03-31 17:46:56 -0700

    wow i need to be better about acp

---
+++ test/tests.js
@@ -0,0 +1,23 @@
+
+import compareNumbers from '../compareNumbers.js';
+
+const test = QUnit.test;
+
+test('testing that returns are 0, 1 and -1', function(assert) {
+    const guess1 = 9;
+    const guess2 = 4;
+    const guess3 = 7;
+    const number = 7;
+
+    const answer = compareNumbers(guess1, number);
+    const answer2 = compareNumbers(guess2, number);
+    const answer3 = compareNumbers(guess3, number);
+
+    // if the guess and correctNumber are identical, return 0
+    // if the guess is lower, return -1
+    // if the guess is higher, return 1
+
+    assert.equal(answer, 1);
+    assert.equal(answer2, -1);
+    assert.equal(answer3, 0);
+});
