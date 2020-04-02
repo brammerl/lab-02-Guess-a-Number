7b34c47c38e5ca1e4b130a48d39d778eb1867a35
Hunter Danielson <hunterleedanielson@gmail.com>
2020-04-02 16:28:35 -0700

    throw error and testing error

--- compareNumbers.js
+++ compareNumbers.js
@@ -1,12 +1,13 @@
 function compareNumbers(guess, correctNumber) {
     if (guess === correctNumber) {
         return 0;
-    } else if (guess < correctNumber) {
+    } else if (guess < correctNumber && guess > 0) {
         return -1;
-    } else if (guess > correctNumber) {
+    } else if (guess > correctNumber && guess < 11) {
         return 1;
+    } else {
+        () => { throw new 'Error: Not a number between 1 and 10'; };
     }
 }
-
 
 export default compareNumbers; 
